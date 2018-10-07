131c1354be5a2c25a58b7fe3d770b1ce33cc768a
Phil <philthompson22@gmail.com>
2018-10-07 12:27:11 -0700

    Add logo and favicon images

--- gen/header.sh
+++ gen/header.sh
@@ -12,6 +12,14 @@ cat << xxxxxEOFxxxxx
 		<meta charset="utf-8">
 		<meta name="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="apple-touch-icon-precomposed" sizes="144×144" href="${SITE_ROOT_REL}/icon-ipad-144x144.png">
+		<link rel="apple-touch-icon-precomposed" sizes="114×114" href="${SITE_ROOT_REL}/icon-iphone-114x144.png">
+		<link rel="apple-touch-icon-precomposed" href="${SITE_ROOT_REL}/icon-default-57x57.png">
+		<link rel="icon" type="image/png" href="${SITE_ROOT_REL}/favicon-16x16.png" sizes="16x16">
+		<link rel="icon" type="image/png" href="${SITE_ROOT_REL}/favicon-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="${SITE_ROOT_REL}/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="${SITE_ROOT_REL}/favicon-64x64.png" sizes="64x64">
+		<link rel="icon" type="image/png" href="${SITE_ROOT_REL}/favicon-32x32.png" sizes="32x32">
 		<link rel="stylesheet" href="${SITE_ROOT_REL}/css/normalize-8.0.0.css">
 		<link rel="stylesheet" href="${SITE_ROOT_REL}/css/style.css">
 	</head>
