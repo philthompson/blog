6e646b66fd85c154a9a2e6e1e5516c1d1522c5c7
Phil <philthompson22@gmail.com>
2018-10-07 12:33:51 -0700

    Remove literal index.html URIs

--- gen/footer.sh
+++ gen/footer.sh
@@ -7,7 +7,7 @@ SITE_ROOT_REL="${2}"
 
 cat << xxxxxEOFxxxxx
 				<div class="copyright">
-					<p>© 2018-${CURRENT_YEAR} <a href="${SITE_ROOT_REL}/index.html">Phil Thompson</a></p>
+					<p>© 2018-${CURRENT_YEAR} <a href="${SITE_ROOT_REL}/">Phil Thompson</a></p>
 				</div>
 			</footer>
 		</div>
