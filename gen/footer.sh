278244d8ff964da5244349010ed95ebf54abee33
Phil <philthompson22@gmail.com>
2018-11-15 06:51:36 -0800

    Add terms, privacy policy, and disclaimer

--- gen/footer.sh
+++ gen/footer.sh
@@ -7,7 +7,15 @@ SITE_ROOT_REL="${2}"
 
 cat << xxxxxEOFxxxxx
 				<div class="copyright">
-					<p>© 2018-${CURRENT_YEAR} <a href="${SITE_ROOT_REL}/">Phil Thompson</a></p>
+					<p>
+						© 2018-${CURRENT_YEAR} <a href="${SITE_ROOT_REL}/about">Phil Thompson</a>
+						&#x2027;
+						<a href="${SITE_ROOT_REL}/terms">Terms and Conditions</a>
+						&#x2027;
+						<a href="${SITE_ROOT_REL}/privacy">Privacy Policy</a>
+						&#x2027;
+						<a href="${SITE_ROOT_REL}/disclaimer">Disclaimer</a>
+					</p>
 				</div>
 			</footer>
 		</div>
