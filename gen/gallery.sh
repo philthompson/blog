b7f1ddb030f2eaac1ad8b88de5ff09a37bbfc159
Phil <phil@philthompson.me>
2021-02-12 13:00:40 -0800

    Add shoot favorites, collapsible copyright info

--- gen/gallery.sh
+++ gen/gallery.sh
@@ -10,20 +10,15 @@
 # shoot:title:<title>
 # shoot:description:<description>
 # shoot:visible:<true|false>
+# shoot:favorite:<photo-name>
 
 #
 # TODO
-# - if shoot description is "--" then don't display it (i thought i did this already)
-# - display each shoot's images in chronological order, not reverse chronological
-# - allow "favorite" image from a shoot to be specified, and floated left inside the
-#     description <p> element
-# - fix top-of-page prev/next button div to include title in middle, nicely, so:
-#     [prev btn](left-align)  ...   title (center)  ...   [next btn](right-align)
-# - make <p> containing hash info collapsible, pure css, with inital state as collapsed
+# - header should show Gallery
 # - better gallery
 #     - index page shows first/favorite image from each shoot, with title/date/desc 
 #     - olderX.html is like index, but for prev pages
-#     - shoot pages stay the same
+#     - shoot pages stay the same, except for "gallery home" button alongside prev/next
 #
 
 THIS_SCRIPT="`perl -MCwd -le 'print Cwd::abs_path shift' "${0}"`"
@@ -82,7 +77,10 @@ do
 		# - description (text)
 		# - bch_txid (text)
 		sqlite3 "${HASHES_DB}" "CREATE TABLE photos (path_rel TEXT PRIMARY KEY, local_datetime_lex TEXT, local_datetime_disp TEXT, species_b64 TEXT, description_b64 TEXT, visible TEXT)"
-		sqlite3 "${HASHES_DB}" "CREATE TABLE shoot (title_b64 TEXT, description_b64 TEXT, bch_txid_b64 TEXT, visible TEXT)"
+		sqlite3 "${HASHES_DB}" "CREATE TABLE shoot (title_b64 TEXT, description_b64 TEXT, bch_txid_b64 TEXT, favorite_path_rel TEXT, default_favorite_path_rel TEXT, visible TEXT)"
+
+		SHOOT_DEFAULT_FAVORITE="`echo "--" | base64`"
+		SHOOT_DEFAULT_FAVORITE_STARS=0
 
 		grep "\-sm\." "${HASHES_FILE}" | while read LINE
 		do
@@ -91,18 +89,25 @@ do
 			PHOTO_PATH_REL="${HASHES_DIR_REL}/${PHOTO_FILENAME}"
 			PHOTO_DATETIME_LEX="`/usr/local/bin/exiftool -d '%Y-%m-%d-%H%M%S' -DateTimeOriginal -S -s "${PHOTO_PATH}"`"
 			PHOTO_DATETIME_DISP="`/usr/local/bin/exiftool -d '%I:%M%p %A %B %d, %Y' -DateTimeOriginal -S -s "${PHOTO_PATH}"`"
-			# i lowercased all species names, so now i have to re-capialize them here
-			# thanks to https://stackoverflow.com/a/1538818/259456 where "ghoti" in
-			#   the comments gave the nice perl one-liner that DOES capitalize the
-			#   char following a hyphen in hyphenated words, so all i need to do with
-			#   sed afterwards is make each apostrophe-'S' into an apostrophe-'s'
-			#   
-			# the other answer (https://stackoverflow.com/a/1541178/259456) almost worked
-			#   but this one properly capitalized everything except letters after
-			#   hyphenated words
-			PHOTO_SPECIES="`/usr/local/bin/exiftool "-MWG:Keywords" -S -s "${PHOTO_PATH}" | sed 's/species://g' | tr 'A-Z' 'a-z' | perl -pe 's/\b(.)/\u\1/g' | sed "s/'S /'s /g" | base64`"
+			PHOTO_STARS="`/usr/local/bin/exiftool -d '%I:%M%p %A %B %d, %Y' -DateTimeOriginal -S -s "${PHOTO_PATH}"`"
+			# i lowercased all species names, so now i have to re-capialize them here...
+			# thanks to https://stackoverflow.com/a/1541178/259456 for the awk script that
+			#   properly capitalizes things at word boundaries
+			# NEVERMIND capitalization isn't apparently uniform for hyphenated words when
+			#   it comes to bird species, so i'm going to leave capitalization of "species:"
+			#   keywords unchanged -- i'll have to capitalize them properly when initially
+			#   tagging them
+			#PHOTO_SPECIES="`/usr/local/bin/exiftool "-MWG:Keywords" -S -s "${PHOTO_PATH}" | sed 's/, /@/g' | tr '@' '\n' | grep species | sed 's/species://g' | tr 'A-Z' 'a-z' | awk '{for(i=1;i<=NF;i++){ $i=toupper(substr($i,1,1)) substr($i,2) }}1' | tr '\n' ',' | sed -e 's/,/, /g' -e 's/, $//' | base64`"
+			PHOTO_SPECIES="`/usr/local/bin/exiftool "-MWG:Keywords" -S -s "${PHOTO_PATH}" | sed 's/, /@/g' | tr '@' '\n' | grep species | sed 's/species://g' | tr '\n' ',' | sed -e 's/,/, /g' -e 's/, $//' | base64`"
 			PHOTO_DESCRIPTION="`echo "--" | base64`"
 			PHOTO_VISIBLE="true"
+			PHOTO_STARS="`/usr/local/bin/exiftool "-MWG:Keywords" -S -s "${PHOTO_PATH}" | sed 's/, /@/g' | tr '@' '\n' | grep -m 1 "stars:[0-9]" | sed -E 's/.*stars:([0-9]).*/\1/'`"
+
+			if [ ! -z "${PHOTO_STARS}" ] && [ $PHOTO_STARS -gt $SHOOT_DEFAULT_FAVORITE_STARS ]
+			then
+				SHOOT_DEFAULT_FAVORITE_STARS=$PHOTO_STARS
+				SHOOT_DEFAULT_FAVORITE="`echo "${PHOTO_PATH_REL}" | base64`"
+			fi
 
 			# if the hashes file supplement exists, replace any photo data with anything
 			#   found in that file
@@ -137,8 +142,9 @@ do
 		SHOOT_TITLE="`echo "${HASHES_ID}" | cut -d '-' -f 2-`"
 		SHOOT_TITLE="`echo "Shoot ${SHOOT_TITLE}" | base64`"
 		SHOOT_DESCRIPTION="`echo "--" | base64`"
-		SHOOT_TXID="${SHOOT_DESCRIPTION}"
+		SHOOT_TXID="`echo "--" | base64`"
 		SHOOT_VISIBLE="true"
+		SHOOT_FAVORITE="`echo "--" | base64`"
 
 		if [ -s "${HASHES_TXID}" ]
 		then
@@ -149,6 +155,7 @@ do
 			SUPP_TITLE="`grep -m 1 "shoot:title:" "${HASHES_SUPP}" | cut -d ':' -f 3-`"
 			SUPP_DESCRIPTION="`grep -m 1 "shoot:description:" "${HASHES_SUPP}" | cut -d ':' -f 3-`"
 			SUPP_VISIBLE="`grep -m 1 "shoot:visible:" "${HASHES_SUPP}" | cut -d ':' -f 3 | tr 'A-Z' 'a-z'`"
+			SUPP_FAVORITE="`grep -m 1 "shoot:favorite:" "${HASHES_SUPP}" | cut -d ':' -f 3`"
 			if [ ! -z "${SUPP_TITLE}" ]
 			then
 				SHOOT_TITLE="`echo "${SUPP_TITLE}" | base64`"
@@ -161,9 +168,13 @@ do
 			then
 				SHOOT_VISIBLE="${SUPP_VISIBLE}"
 			fi
+			if [ ! -z "${SUPP_FAVORITE}" ]
+			then
+				SHOOT_FAVORITE="`echo "${HASHES_DIR_REL}/${SUPP_FAVORITE}" | base64`"
+			fi
 		fi
 
-		sqlite3 "${HASHES_DB}" "INSERT INTO shoot (title_b64, description_b64, bch_txid_b64, visible) VALUES ('${SHOOT_TITLE}', '${SHOOT_DESCRIPTION}', '${SHOOT_TXID}', '${SHOOT_VISIBLE}');"
+		sqlite3 "${HASHES_DB}" "INSERT INTO shoot (title_b64, description_b64, bch_txid_b64, favorite_path_rel, default_favorite_path_rel, visible) VALUES ('${SHOOT_TITLE}', '${SHOOT_DESCRIPTION}', '${SHOOT_TXID}', '${SHOOT_FAVORITE}', '${SHOOT_DEFAULT_FAVORITE}', '${SHOOT_VISIBLE}');"
 	fi
 
 	# build the page for this hashes file
@@ -187,47 +198,69 @@ do
 		GALLERY_PAGE="${GALLERY_OUT}/gallery-`echo "${HASHES_ID}" | cut -d '-' -f 2-`.html"
 	fi
 
-	SHOOT_DATA="`sqlite3 "${HASHES_DB}" "SELECT title_b64, description_b64, bch_txid_b64, visible FROM shoot LIMIT 1;"`"
+	SHOOT_DATA="`sqlite3 "${HASHES_DB}" "SELECT title_b64, description_b64, bch_txid_b64, favorite_path_rel, default_favorite_path_rel, visible FROM shoot LIMIT 1;"`"
 	SHOOT_TITLE="`echo "${SHOOT_DATA}" | cut -d '|' -f 1 | base64 -d`"
 	SHOOT_DESC="`echo "${SHOOT_DATA}" | cut -d '|' -f 2 | base64 -d`"
 	SHOOT_TXID="`echo "${SHOOT_DATA}" | cut -d '|' -f 3 | base64 -d`"
-	SHOOT_VISIBLE="`echo "${SHOOT_DATA}" | cut -d '|' -f 4`"
+	SHOOT_FAVORITE="`echo "${SHOOT_DATA}" | cut -d '|' -f 4 | base64 -d`"
+	SHOOT_DEFAULT_FAVORITE="`echo "${SHOOT_DATA}" | cut -d '|' -f 5 | base64 -d`"
+	SHOOT_VISIBLE="`echo "${SHOOT_DATA}" | cut -d '|' -f 6`"
 	if [ "${SHOOT_VISIBLE}" != "true" ]
 	then
 		echo "shoot ${HASHES_ID} is not visible (${SHOOT_VISIBLE}), so skipping"
 		continue
 	fi
-	if [ "${SHOOT_DESC}" != "--" ]
+	if [ ! -z "${SHOOT_DESC}" ] && [ "${SHOOT_DESC}" != "--" ]
 	then
 		SHOOT_DESC="<p>${SHOOT_DESC}</p>"
+	else
+		SHOOT_DESC=""
 	fi
+	if [ ! -z "${SHOOT_FAVORITE}" ] && [ "${SHOOT_FAVORITE}" != "--" ]
+	then
+		SHOOT_FAVORITE="<a href=\"${SHOOT_FAVORITE}\"><img style=\"float: left\" class=\"width-resp-50-100\" src=\"${SHOOT_FAVORITE}\"/></a>"
+	else
+		SHOOT_FAVORITE=""
+	fi
+	SHOOT_YEAR="`echo "${HASHES_ID}" | cut -d '-' -f 2`"
 
 	"${HEADER_SCRIPT}" "${SHOOT_TITLE}" "${SITE_ROOT_REL}" "bird photos gallery" "description here" 14 > "${GALLERY_PAGE}"
 
+# wow thanks to https://stackoverflow.com/a/15742338/259456 for pointing out that
+#   HTML now has <details> and <summary> tags with auto-collapse!
 cat << xxxxxEOFxxxxx >> "${GALLERY_PAGE}"
-<div class="btns" style="margin:0">
+<div class="btns" style="margin:0 0 2rem 0">
 	xxxxxPREVBUTTONxxxxx
 	xxxxxNEXTBUTTONxxxxx
 </div>
-<div class="container">
-	<h1 class="article-title">${SHOOT_TITLE}</h1>
+<div class="">
+	<span class="article-title">${SHOOT_TITLE}</span>
+	${SHOOT_FAVORITE}
 	${SHOOT_DESC}
-	<p><a target="_blank" href="${HASHES_DIR_REL}/${HASHES_ID}.txt">📄 ${HASHES_ID}.txt</a></p>
-	<p><a target="_blank" href="${HASHES_DIR_REL}/${HASHES_ID}.txt.sig">📄 ${HASHES_ID}.txt.sig</a></p>
-	<p><small>The above <code>hashes-&lt;date&gt;.txt</code> file contains hashes of all the photos from this shoot,
-	both the larger, full-quality images and the below smaller images.  The <code>hashes-&lt;date&gt;.txt.sig</code>
-	is a signature of that hashes file, created with <a href="${SITE_ROOT_REL}/about">my PGP key</a>.  The
-	signature file itself was written to the BCH blockchain, in <code>OP RETURN</code> data for the
-	transaction below.  See this blog post for more details.</small></p>
-	<p class="article-info"><a target="_blank" href="https://www.blockchain.com/bch/tx/${SHOOT_TXID}">view on blockchain.com: ${SHOOT_TXID}</a></p>
-	<p class="article-info"><a target="_blank" href="https://blockchair.com/bitcoin-cash/transaction/${SHOOT_TXID}">view on blockchair.com: ${SHOOT_TXID}</a></p>
-	<p class="article-info"><a target="_blank" href="https://explorer.bitcoin.com/bch/tx/${SHOOT_TXID}">view on bitcoin.com: ${SHOOT_TXID}</a></p>
+	<details>
+		<summary>Copyright and Signature</summary>
+		<p>These photos are © ${SHOOT_YEAR} Phil Thompson, all rights reserved.</p>
+		<p><a target="_blank" href="${HASHES_DIR_REL}/${HASHES_ID}.txt">📄 ${HASHES_ID}.txt</a></p>
+		<p><a target="_blank" href="${HASHES_DIR_REL}/${HASHES_ID}.txt.sig">📄 ${HASHES_ID}.txt.sig</a></p>
+		<p>
+			The above <code>hashes-&lt;date&gt;.txt</code> file contains SHA-256 hashes of all the photos
+			from this shoot.  The <code>hashes-&lt;date&gt;.txt.sig</code> is a signature of that hashes
+			file, created with <a href="${SITE_ROOT_REL}/about">my PGP key</a>.  The signature file
+			itself was written to the BCH blockchain, in <code>OP RETURN</code> data for the transaction
+			below.  In short, this proves that these photos and the signature both existed at the time
+			the transaction was written to the blockchain.  A future blog post will have more details.
+		</p>
+		<p class="article-info"><a target="_blank" href="https://blockchair.com/bitcoin-cash/transaction/${SHOOT_TXID}">view on blockchair.com: ${SHOOT_TXID}</a></p>
+	</details>
+	<p style="clear:both;"></p>
 </div>
 xxxxxEOFxxxxx
+# <p class="article-info"><a target="_blank" href="https://www.blockchain.com/bch/tx/${SHOOT_TXID}">view on blockchain.com: ${SHOOT_TXID}</a></p>
+# <p class="article-info"><a target="_blank" href="https://explorer.bitcoin.com/bch/tx/${SHOOT_TXID}">view on bitcoin.com: ${SHOOT_TXID}</a></p>
 
 	# thanks to https://stackoverflow.com/a/51863033/259456 for row_number
 	#sqlite3 "${HASHES_DB}" "SELECT ROW_NUMBER() OVER(ORDER BY local_datetime_lex) AS rnum, path_rel, local_datetime_lex, local_datetime_disp, species_b64, description_b64 FROM photos ORDER BY local_datetime_lex DESC;" | while read LINE
-	sqlite3 "${HASHES_DB}" "SELECT path_rel, local_datetime_lex, local_datetime_disp, species_b64, description_b64, visible FROM photos ORDER BY local_datetime_lex DESC;" | while read LINE
+	sqlite3 "${HASHES_DB}" "SELECT path_rel, local_datetime_lex, local_datetime_disp, species_b64, description_b64, visible FROM photos ORDER BY local_datetime_lex ASC;" | while read LINE
 	do
 		#echo "${HASHES_ID} photo: ${LINE}"
 		PHOTO_PATH="`echo "${LINE}" | cut -d '|' -f 1`"
