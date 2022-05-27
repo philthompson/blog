8c0e6292c45e65b421fba15cbeac2bbbb1fcb2e4
Phil <phil@philthompson.me>
2022-05-27 07:56:44 -0700

    Generate per-year favorites summery pages

--- gen/generateAllBirdsPages.sh
+++ gen/generateAllBirdsPages.sh
@@ -9,14 +9,25 @@ BIRDS_STATIC_DIR="${THIS_DIR}/static/birds"
 
 echo "generating all all ..." &&
 python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" all all > "${BIRDS_STATIC_DIR}/index.md" &&
-echo "generating 2021 all ..." &&
-python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" 2021 all > "${BIRDS_STATIC_DIR}/2021.md" &&
-echo "generating 2022 all ..." &&
-python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" 2022 all > "${BIRDS_STATIC_DIR}/2022.md" &&
 echo "generating all loc_home_1 ..." &&
-python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" all loc_home_1 > "${BIRDS_STATIC_DIR}/home.md" &&
-echo "generating 2021 loc_home_1 ..." &&
-python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" 2021 loc_home_1 > "${BIRDS_STATIC_DIR}/home-2021.md" &&
-echo "generating 2022 loc_home_1 ..." &&
-python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" 2022 loc_home_1 > "${BIRDS_STATIC_DIR}/home-2022.md"
+python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" all loc_home_1 > "${BIRDS_STATIC_DIR}/home.md"
+
+if [ $? -ne 0 ]
+then
+	exit
+fi
+
+seq 2021 2022 | while read YEAR
+do
+	echo "generating ${YEAR} all ..." &&
+	python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" "${YEAR}" all > "${BIRDS_STATIC_DIR}/${YEAR}.md" &&
+	echo "generating ${YEAR} loc_home_1 ..." &&
+	python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" "${YEAR}" loc_home_1 > "${BIRDS_STATIC_DIR}/home-${YEAR}.md" &&
+	echo "generating ${YEAR} fav ..." &&
+	python3 "${THIS_DIR}/generateBirdsPage.py" "${BIRDS_DB}" "${YEAR}" fav > "${BIRDS_STATIC_DIR}/favorites-${YEAR}.md"
+	if [ $? -ne 0 ]
+	then
+		exit
+	fi
+done
 
